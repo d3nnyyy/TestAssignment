8002810ea29c82bbce0857bfeb39af6da8a8457a
d3nnyyy <d3nnn41k@gmail.com>
2023-09-28 16:17:16 +0300

    created getUserByBirthdayRange method and query to it

--- src/main/java/ua/dtsebulia/testassignment/repository/UserRepository.java
+++ src/main/java/ua/dtsebulia/testassignment/repository/UserRepository.java
@@ -1,10 +1,23 @@
 package ua.dtsebulia.testassignment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.dtsebulia.testassignment.model.User;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u " +
+            "FROM User u " +
+            "WHERE u.dateOfBirth " +
+            "BETWEEN :fromDate AND :toDate")
+    List<User> findUsersByBirthdayRange(
+            @Param("fromDate") Date fromDate,
+            @Param("toDate") Date toDate
+    );
 }
