2140a65ee2a5eea54783712ac2c05478f5f76de6
d3nnyyy <d3nnn41k@gmail.com>
2023-09-28 14:07:40 +0300

    added post endpoint for creating user

--- src/main/java/ua/dtsebulia/testassignment/controller/UserController.java
+++ src/main/java/ua/dtsebulia/testassignment/controller/UserController.java
@@ -1,16 +1,17 @@
 package ua.dtsebulia.testassignment.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ua.dtsebulia.testassignment.exception.UserAlreadyExistsException;
 import ua.dtsebulia.testassignment.exception.UserNotFoundException;
 import ua.dtsebulia.testassignment.model.User;
 import ua.dtsebulia.testassignment.service.UserService;
+
+import java.text.ParseException;
 
 @RequiredArgsConstructor
 @RestController
@@ -37,6 +38,20 @@ public class UserController {
             return ResponseEntity
                     .status(HttpStatus.NOT_FOUND)
                     .body("User not found with id: " + id);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody @Valid User user) throws ParseException {
+        try {
+            log.info("Creating user {}", user);
+            User createdUser = userService.createUser(user);
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        } catch (UserAlreadyExistsException ex) {
+            log.error("User with email {} already exists", user.getEmail());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("User with email " + user.getEmail() + " already exists");
         }
     }
 
--- src/main/java/ua/dtsebulia/testassignment/service/UserService.java
+++ src/main/java/ua/dtsebulia/testassignment/service/UserService.java
@@ -2,6 +2,7 @@ package ua.dtsebulia.testassignment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ua.dtsebulia.testassignment.exception.UserAlreadyExistsException;
 import ua.dtsebulia.testassignment.exception.UserNotFoundException;
 import ua.dtsebulia.testassignment.model.User;
 import ua.dtsebulia.testassignment.repository.UserRepository;
@@ -22,5 +23,16 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(
                 () -> new UserNotFoundException("User not found with id: " + id)
         );
+    }
+
+    public User createUser(User user) {
+
+        String email = user.getEmail();
+
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new UserAlreadyExistsException("User with email " + email + " already exists");
+        }
+
+        return userRepository.save(user);
     }
 }
