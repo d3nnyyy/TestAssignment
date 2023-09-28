13d1d65d7f407f3c53705ec6daf4ad337d9c2607
d3nnyyy <d3nnn41k@gmail.com>
2023-09-28 14:07:18 +0300

    added findByEmail method and exception if user with this email exists

--- src/main/java/ua/dtsebulia/testassignment/repository/UserRepository.java
+++ src/main/java/ua/dtsebulia/testassignment/repository/UserRepository.java
@@ -3,5 +3,8 @@ package ua.dtsebulia.testassignment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.dtsebulia.testassignment.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
